3292c177432bcc016ef8c504a5fe52ed4353b867
Thaplong <princemabuza0@gmail.com>
2024-10-19 22:04:15 +0200

    Add files via upload

---
+++ 2. select statement Tutoria by Thaplong.sql
@@ -0,0 +1,18 @@
+Select *
+From parks_and_recreation.employee_demographics;
+
+Select first_name,
+ last_name,
+ birth_date,
+ age,
+ (age + 10) * 10 + 10 
+From parks_and_recreation.employee_demographics;
+# PEMDAS 
+
+SELECT DISTINCT first_name, gender
+From parks_and_recreation.employee_demographics;
+
+
+
+
+
